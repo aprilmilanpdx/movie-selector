e142462c269d4df44d1983d143543d39167b5201
April Milan <108895508+aprilmilanpdx@users.noreply.github.com>
2023-02-25 10:32:24 -0800

    working code except for find_movie

--- milestone_project_1.py
+++ milestone_project_1.py
@@ -1,10 +1,7 @@
-# Incomplete app!
-
 MENU_PROMPT = "\nEnter 'a' to add a movie, 'l' to see your movies, 'f' to find a movie by title, or 'q' to quit: "
 movies = []
 
 
-# You may want to create a function for this code
 def movie_info():
   title = input("Enter the movie title: ")
   director = input("Enter the movie director: ")
@@ -15,14 +12,16 @@ def movie_info():
       'director': director,
       'year': year
   })
+  
 
 
-# Create other functions for:
-#   - listing movies
+def list_movies():
+   for movie in movies:
+      print(movie['title'])
+
 #   - finding movies
 
 
-# And another function here for the user menu
 def menu_selector():
   selection = input(MENU_PROMPT)
   while selection != 'q':
@@ -30,8 +29,8 @@ def menu_selector():
           # pass
           movie_info()
       elif selection == "l":
-          pass
-          # list_movies()
+          # pass
+          list_movies()
       elif selection == "f":
           pass
           # find_movie()
@@ -40,6 +39,4 @@ def menu_selector():
 
       selection = input(MENU_PROMPT)
 
-
-# Remember to run the user menu function at the end!
 menu_selector()
